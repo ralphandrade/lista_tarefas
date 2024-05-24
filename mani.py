a47902192b4eb1fee8c0dd716e078a68297a6efc
ralphandrade <ralphandrade06@gmail.com>
2024-05-24 20:06:59 -0300

    lista_tarefas

---
+++ mani.py
@@ -0,0 +1,19 @@
+# lista de tarefas
+tarefas = []
+
+# laço de repetição
+while True:
+# usuario informa intem da tarefa
+    nova_tarefa = input('Informe a nova tarefa ou deixe vazio para encerrar e exibir a lista: ')
+
+# verificase o usuario inseriu nova tarefa
+    if nova_tarefa !='':
+        tarefas.append(nova_tarefa)
+        continue
+    else:
+        break
+
+# exibe a lista de tarefas
+for tarefa in tarefas:
+    print(tarefa)
+
